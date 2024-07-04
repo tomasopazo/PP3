726724975a52f229cb2cacd417cff29c49f244c7
Nacho2001 <nachomigoni@gmail.com>
2024-07-04 11:51:10 -0300

    my-app4 cargado

---
+++ modelos/my-app4/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB24Gzomz2KLIUtj3kU81egmmjcminSP7Y",
+    authDomain: "react-native-demo-8db5c.firebaseapp.com",
+    projectId: "react-native-demo-8db5c",
+    storageBucket: "react-native-demo-8db5c.appspot.com",
+    messagingSenderId: "617443873545",
+    appId: "1:617443873545:web:404b6019aef77ea12779d8",
+    measurementId: "G-PM3XJ4V29F"
+  };
+  export default firebaseConfig;
