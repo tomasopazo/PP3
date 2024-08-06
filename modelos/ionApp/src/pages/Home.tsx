73768abe43ad01c1c22a032dfee8dabc7d980b51
Nacho2001 <nachomigoni@gmail.com>
2024-08-06 15:27:22 -0300

    Completar Home.tsx

--- modelos/ionApp/src/pages/Home.tsx
+++ modelos/ionApp/src/pages/Home.tsx
@@ -1,8 +1,26 @@
+import React, {useRef, useEffect} from 'react';
+import 'aframe';
+import db from '../config/firebaseConfig';
+import { doc, onSnapshot, setDoc } from 'firebase/firestore';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
+const getRandomColor = (): string => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
 const Home: React.FC = () => {
+  const sphereRef = useRef<any>(null);
+
+  useEffect(() => {
+    const sphereDocRef = doc(db, 'objects', 'my-sphere');
+  })
   return (
     <IonPage>
       <IonHeader>
